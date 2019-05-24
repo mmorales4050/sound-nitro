c04d720a39039223209a4e4067e4d878e78451ac
mmorales4050 <mmorales4050@gmail.com>
2019-05-24 11:56:41 -0400

    updated reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 
 let initialState = {
   files: [],
@@ -12,45 +13,124 @@ let initialState = {
   selectedSong: null
 }
 
-const filesReducer = (state=[], action) => {
-
-}
-
-const rootReducer = (oldState=initialState, action) => {
+const rootReducer = (state=initialState, action) => {
   switch(action.type){
     case "SELECT_SONG":
-      return {...oldState, selectedSong: action.payload}
+      return {...state, selectedSong: action.payload}
     case "DISPLAY_PLAYLIST":
-      return {...oldState, displayPlaylist: action.payload}
+      return {...state, displayPlaylist: action.payload}
     case "SET_PLAYLISTS":
-      return {...oldState, playlists: action.payload}
+      return {...state, playlists: action.payload}
     case "SET_DURATION":
-      return {...oldState, track_duration: oldState.current_track.howl.duration()}
+      return {...state, track_duration: state.current_track.howl.duration()}
     case "TOGGLE_AUDIO":
-      if (oldState.playing){
-        oldState.current_track.howl.pause()
+      if (state.playing){
+        state.current_track.howl.pause()
       }else{
-        oldState.current_track.howl.play()
+        state.current_track.howl.play()
       }
-      return {...oldState, playing: !oldState.playing}
+      return {...state, playing: !state.playing}
     case "CHANGE_PAGE":
-      return {...oldState, page: action.payload}
+      return {...state, page: action.payload}
     case "CHOOSE_FILES_TO_UPLOAD":
-      return {...oldState, files: action.payload}
+      return {...state, files: action.payload}
     case "CLEAR_FILES_TO_UPLOAD":
-      return {...oldState, files: []}
+      return {...state, files: []}
     case "SET_SONGS":
-      return {...oldState, songs: action.payload}
+      return {...state, songs: action.payload}
       case "SET_AND_PLAY_CURRENT_TRACK":
         // Pause song if there is currently one playing
-        try{oldState.current_track.howl.pause()}catch(e){}
+        try{state.current_track.howl.pause()}catch(e){}
         action.payload.howl.play()
-        return {...oldState, current_track: action.payload, playing: true}
+        return {...state, current_track: action.payload, playing: true}
       case "SET_QUEUE":
-        return {...oldState, queue: action.payload}
+        return {...state, queue: action.payload}
     default:
-      return oldState
+      return state
   }
 }
+
+const filesReducer = (state=[], action) => {
+  switch(action.type){
+    case "ADD_FILES":
+      return {...state, files: action.payload}
+    default:
+      return state
+    }
+}
+const playlistsReducer = (state=[], action) => {
+  switch(action.type){
+    case "ADD_PLAYLISTS":
+      return {...state, playlists: action.payload}
+    default:
+      return state
+    }
+}
+const queueReducer = (state=null, action) => {
+  switch(action.type){
+    case "ADD_QUEUE":
+      return {...state, queue: action.payload}
+    default:
+      return state
+    }
+}
+const displayPlaylistReducer = (state=null, action) => {
+  switch(action.type){
+    case "ADD_DISPLAYPLAYLIST":
+      return {...state, displayPlaylist: action.payload}
+    default:
+      return state
+    }
+}
+const songsReducer = (state=[], action) => {
+  switch(action.type){
+    case "ADD_SONGS":
+      return {...state, songs: action.payload}
+    default:
+      return state
+    }
+}
+const current_trackReducer = (state=null, action) => {
+  switch(action.type){
+    case "ADD_CURRENT_TRACK":
+      return {...state, current_track: action.payload}
+    default:
+      return state
+    }
+}
+const playingReducer = (state=false, action) => {
+  switch(action.type){
+    case "ADD_PLAYING":
+      return {...state, playing: action.payload}
+    default:
+      return state
+    }
+}
+const pageReducer = (state="songs", action) => {
+  switch(action.type){
+    case "ADD_PAGE":
+      return {...state, page: action.payload}
+    default:
+      return state
+    }
+}
+const track_durationReducer = (state=0, action) => {
+  switch(action.type){
+    case "ADD_TRACK_DURATION":
+      return {...state, track_duration: action.payload}
+    default:
+      return state
+    }
+}
+const selectedSongReducer = (state=null, action) => {
+  switch(action.type){
+    case "ADD_SELECTEDSONG":
+      return {...state, selectedSong: action.payload}
+    default:
+      return state
+    }
+}
+
+
 
 export default rootReducer
